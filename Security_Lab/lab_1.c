02fab9c685277f05dc5b82d084538274b14cce8d
DaronD1709 <110324724+DaronD1709@users.noreply.github.com>
2024-10-21 09:51:16 +0700

    lab_01 21Oct

---
+++ Security_Lab/lab_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+void redundant_code(char* p)
+{
+    char local[256];
+    strncpy(local,p,20);
+	printf("redundant code\n");
+}
+int main(int argc, char* argv[])
+{
+	char buffer[16];
+	strcpy(buffer,argv[1]);
+	return 0;
+}
